b6e1d95b3f256d0fb05f2058424fcb33c3f9002f
gustavodev200 <gustavolage200@gmail.com>
2022-04-11 23:06:02 -0300

    Deletando e editando tarefas

--- backend/controllers/TaskController.js
+++ backend/controllers/TaskController.js
@@ -2,7 +2,7 @@ const Task = require("../models/Task");
 
 const getToken = require("../helpers/get-token");
 const getUserByToken = require("../helpers/get-user-by-token");
-const ObejectId = require("mongoose").Types.ObjectId;
+const ObjectId = require("mongoose").Types.ObjectId;
 
 module.exports = class TaskController {
   static async createTasks(req, res) {
@@ -44,5 +44,89 @@ module.exports = class TaskController {
     } catch (error) {
       res.status(500).json({ message: error });
     }
+  }
+
+  static async removeTasks(req, res) {
+    const id = req.params.id;
+
+    //check if id is valid
+    if (!ObjectId.isValid(id)) {
+      res.status(422).json({ message: "ID inválido!" });
+      return;
+    }
+
+    //check if task exist in my database
+    const task = await Task.findOne({ _id: id });
+
+    if (!task) {
+      res.status(404).json({ message: "Tarefa não encontrada!" });
+      return;
+    }
+
+    //check if logged in user registered the task
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    if (task.user._id.toString() !== user._id.toString()) {
+      res.status(422).json({
+        message:
+          "Houve um problema em processar a sua solicitação, tente novamente mais tarde!",
+      });
+      return;
+    }
+
+    await Task.findByIdAndRemove(id);
+
+    res.status(200).json({ message: "Tarefa removida com sucesso!" });
+  }
+
+  static async editTasks(req, res) {
+    const id = req.params.id;
+
+    const { task, taskpriority } = req.body;
+
+    const updateData = {};
+
+    //check if task exists
+    const tasks = await Task.findOne({ _id: id });
+
+    if (!tasks) {
+      res.status(404).json({ message: "Tarefa não encontrada!" });
+      return;
+    }
+
+    //check if logged in user registered the task
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    if (tasks.user._id.toString() !== user._id.toString()) {
+      res.status(422).json({
+        message:
+          "Houve um problema em processar a sua solicitação, tente novamente mais tarde!",
+      });
+      return;
+    }
+
+    if (!task) {
+      res
+        .status(422)
+        .json({ message: "A tarefa é obrigatória, Por favor digite algo... " });
+      return;
+    } else {
+      updateData.task = task;
+    }
+
+    if (!taskpriority) {
+      res
+        .status(422)
+        .json({ message: "O grau de dificuldade da Tarefa é obrigatória" });
+      return;
+    } else {
+      updateData.taskpriority = taskpriority;
+    }
+
+    await Task.findByIdAndUpdate(id, updateData);
+
+    res.status(200).json({ message: "Tarefa atualizada com sucesso!" });
   }
 };
--- backend/routes/TaskRoutes.js
+++ backend/routes/TaskRoutes.js
@@ -5,5 +5,7 @@ const TaskController = require('../controllers/TaskController')
 const verifyToken = require('../helpers/verify-token')
 
 router.post('/create', verifyToken, TaskController.createTasks)
+router.delete('/:id', verifyToken, TaskController.removeTasks)
+router.patch('/:id', verifyToken, TaskController.editTasks)
 
 module.exports = router;
